9638d885c2d045ee1d461e512078b6832076e9ff
Lena434 <lenalozovanu@gmail.com>
2026-02-10 00:31:15 +0200

    Add Game List Page with filtering functionality

--- web-frontend/src/App.tsx
+++ web-frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import GameListPage from "./pages/GameListPage";  
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/games" element={<GameListPage />} />
       </Route>
     </Routes>
   );
