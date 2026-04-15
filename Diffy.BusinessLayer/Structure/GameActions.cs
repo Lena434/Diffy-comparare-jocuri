c45fff5576c39b99fdf7b194248daade4ec88ee5
Stanislav <stas8puica@gmail.com>
2026-04-16 01:44:43 +0300

    feat(business): add GameActions, GenreActions, PlatformActions, GameModeActions

--- Diffy.BusinessLayer/Structure/GameActions.cs
+++ Diffy.BusinessLayer/Structure/GameActions.cs
@@ -1,104 +1,22 @@
-﻿using Diffy.BusinessLayer.Interfaces;
-using Diffy.DataAccessLayer.Context;
+using Diffy.BusinessLayer.Interfaces;
+using Diffy.DataAccessLayer.Repositories;
 using Diffy.Domain.Entities.Game;
-using Diffy.Domain.Models.Game;
-
 
 namespace Diffy.BusinessLayer.Structure;
 
-public class GameActions
+public class GameActions : IGame
 {
-    private readonly DiffyDbContext _dbContext;
+    private readonly GameRepository _repo;
 
-    public GameActions()
+    public GameActions(GameRepository repo)
     {
-        _dbContext = new DiffyDbContext();
-    }
-    public bool CreateGameAction(GameCreateDto gameCreateDto)
-    {
-        var gameEntity = new GameEntity
-        {
-            Title = gameCreateDto.Title,
-            Genre = gameCreateDto.Genre,
-            Price = gameCreateDto.Price,
-        };
-
-        try
-        {
-            _dbContext.Games.Add(gameEntity);
-            _dbContext.SaveChanges();
-            return true;
-        } catch  (Exception e)
-        {
-            return false;
-        }
+        _repo = repo;
     }
 
-    public GameInfoDto? GetGameByIdAction(int id)
-    {
-        var gameEntity = _dbContext.Games.FirstOrDefault(g => g.Id == id);
-        
-        if (gameEntity == null)
-            return null;
-
-        var gameInfoDto = new GameInfoDto
-        {
-            Id = gameEntity.Id,
-            Title = gameEntity.Title,
-            Genre = gameEntity.Genre,
-            Price = gameEntity.Price,
-        };
-        return gameInfoDto;
-    }
-
-    public List<GameInfoDto> GetGameListAction()
-    {
-        var games = _dbContext.Games.Select(gameEntity => new GameInfoDto
-        {
-            Id = gameEntity.Id,
-            Title = gameEntity.Title,
-            Genre = gameEntity.Genre,
-            Price = gameEntity.Price,
-        }).ToList();
-        return games;
-    }
-
-    public bool UpdateGameAction(int id, GameUpdateDto gameUpdateDto)
-    {
-        var game = _dbContext.Games.FirstOrDefault(g => g.Id == id);
-        if (game == null) return false;
-        
-        game.Title = gameUpdateDto.Title;
-        game.Genre = gameUpdateDto.Genre;
-        game.Price = gameUpdateDto.Price;
-
-        try
-        {
-            _dbContext.SaveChanges();
-            return true;
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
-    }
-
-    public bool DeleteGameAction(int id)
-    {
-        var game = _dbContext.Games.FirstOrDefault(g => g.Id == id);
-        if (game == null) return false;
-
-        try
-        {
-            _dbContext.Games.Remove(game);
-            _dbContext.SaveChanges();
-            return true;
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
-    }
-    
-    
+    public Task<List<GameEntity>> GetAllAsync() => _repo.GetAllAsync();
+    public Task<GameEntity?> GetByIdAsync(int id) => _repo.GetByIdAsync(id);
+    public Task<List<GameEntity>> GetByIdsAsync(List<int> ids) => _repo.GetByIdsAsync(ids);
+    public Task AddAsync(GameEntity game, List<int> genreIds, List<int> platformIds, List<int> gameModeIds) => _repo.AddAsync(game, genreIds, platformIds, gameModeIds);
+    public Task UpdateAsync(GameEntity game, List<int> genreIds, List<int> platformIds, List<int> gameModeIds) => _repo.UpdateAsync(game, genreIds, platformIds, gameModeIds);
+    public Task DeleteAsync(int id) => _repo.DeleteAsync(id);
 }
