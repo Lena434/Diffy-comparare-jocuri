010c629f098147760b35905b78f9465d65e4b2c9
Stanislav <stas8puica@gmail.com>
2026-04-16 01:46:25 +0300

    feat(api): configure JWT auth, CORS, AutoMapper, Swagger Bearer and all service registrations in Program.cs

--- Diffy.Api/Program.cs
+++ Diffy.Api/Program.cs
@@ -1,21 +1,95 @@
+using Diffy.BusinessLayer.Interfaces;
+using Diffy.BusinessLayer.Structure;
+using Diffy.DataAccessLayer.Context;
+using Diffy.DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter: Bearer {your token}"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("LocalhostPolicy", policy =>
+    options.AddPolicy("AllowFrontend", policy =>
     {
-        policy
-            .WithOrigins("http://localhost:5173")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-            
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
+
+builder.Services.AddDbContext<DiffyDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<GenreRepository>();
+builder.Services.AddScoped<PlatformRepository>();
+builder.Services.AddScoped<GameModeRepository>();
+builder.Services.AddScoped<GameRepository>();
+
+builder.Services.AddScoped<IGenre, GenreActions>();
+builder.Services.AddScoped<IPlatform, PlatformActions>();
+builder.Services.AddScoped<IGameMode, GameModeActions>();
+builder.Services.AddScoped<IGame, GameActions>();
+
+builder.Services.AddScoped<UserFavoriteRepository>();
+builder.Services.AddScoped<IUserFavorite, UserFavoriteActions>();
+builder.Services.AddScoped<GameRatingRepository>();
+builder.Services.AddScoped<IGameRating, GameRatingActions>();
+builder.Services.AddScoped<UserProfileRepository>();
+builder.Services.AddScoped<IUserProfile, UserProfileActions>();
+
+builder.Services.AddAutoMapper(typeof(Diffy.Api.MappingProfile));
+
+var jwtKey = builder.Configuration["Jwt:Key"]!;
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 
 var app = builder.Build();
@@ -28,6 +102,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors("LocalhostPolicy");
+app.UseCors("AllowFrontend");
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
